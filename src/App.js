62d60e12085e65c12c43edfc9d1cf48c96ff6359
Rodrigo Nikolof <rodrigo.nikolof@outlook.com>
2022-11-28 08:45:05 +1100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from "react";
+
+
 
 function App() {
+
+
+const [pokemon, setPokemon] = useState('');
+
+useEffect(()=>{
+  fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+  .then(res => res.json())
+  .then(data => setPokemon(data.results))
+}, [setPokemon])
+
+console.log(pokemon)
+// const showPokes =   pokemon.map((poke)=>{
+//   return <p>{poke}</p>
+// })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
     </div>
   );
 }

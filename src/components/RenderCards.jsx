eac8e711304cdf6ebf20196e50783788b9ef203d
Rodrigo Nikolof <rodrigo.nikolof@outlook.com>
2022-12-03 10:32:14 +1100

    Added confrmation message when pokemon is saved to database

--- src/components/RenderCards.jsx
+++ src/components/RenderCards.jsx
@@ -1,10 +1,14 @@
 // import { type } from '@testing-library/user-event/dist/type';
 import React from 'react';
 import { useState, useEffect } from 'react'
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
+
 
 function RenderCards({url}){
 
  const [thisPokemon, setThisPokemon] = useState(null);
+ const [savedMsg, setSavedMsg] = useState(false)
  
 
  useEffect(()=>{
@@ -20,11 +24,15 @@ function RenderCards({url}){
 // types[0].type.name
 
 function handleSave(){
+    setSavedMsg(true)
+    setTimeout(() => {
+      setSavedMsg(false)
+    }, 3000);
     const pokemonData = {
         name : thisPokemon.name,
         pokeId : thisPokemon.id,
-        image : thisPokemon.sprites.front_default,
-        types : thisPokemon.types
+        image : thisPokemon.sprites.front_default
+        // types : thisPokemon.types
       };
       fetch("http://localhost:3000/pokemon", {
         method: "POST",
@@ -37,8 +45,15 @@ function handleSave(){
         .then((pokemon) => console.log(pokemon));
     }
 
+    // const Modal = () => (
+    //   <Popup trigger={<button className="button"> Open Modal </button>} modal>
+    //     <span> Modal content </span>
+    //   </Popup>
+    // );
 
+ 
 
+    
 
     return(
         <div className="pokeCard" >
@@ -47,6 +62,9 @@ function handleSave(){
             <small>{thisPokemon? thisPokemon.types[0].type.name : null}</small>
             {/* <small>{thisPokemon.types.length === 2 ?  thisPokemon.types[1].type.name : null}</small> */}
             <button onClick={handleSave}>Steal Pokemon</button>
+            {savedMsg && <p>Saved</p>}
+           
+            
             
         </div>
     ) 
